3b2473a5ee0e4751912b1db059e685d35341e956
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-05-13 05:41:26 -0400

    okay now lesson one is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Nav from './componets/Nav';
 import About from './componets/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
